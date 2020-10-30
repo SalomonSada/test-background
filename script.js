e358acba3cb1679e192af622f8f62209ef8297e5
Salomon Sada <66238895+SalomonSada@users.noreply.github.com>
2020-10-29 23:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-const room = [1, 2, 3, 1, 2, 1];
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var random = document.querySelector('.random');
 
-// the best until now
+color1.addEventListener('input', change);
+color2.addEventListener('input', change);
+random.addEventListener('click', azar);
+
+function change() {
+  body.style.background =
+    'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+  css.textContent = body.style.background + ';';
+
+  random.style.background =
+    'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+}
+
+function azar() {
+  var color1 = getRandomColor();
+  var color2 = getRandomColor();
+  body.style.background =
+    'linear-gradient(to right, ' + color1 + ', ' + color2 + ')';
+  css.textContent = body.style.background + ';';
+
+  random.style.background =
+    'linear-gradient(to right, ' + color1 + ', ' + color2 + ')';
+  css.textContent = body.style.background + ';';
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+/**const room = [1, 2, 3, 1, 2, 1];
 const cleanRoom1 = room.filter((n) => {
   for (let i = 0; i < room.length; i++) {
     return n === room[i];
   }
 });
-
-// 2
 const add = (a, b) =>
   a.filter((n) => {
     for (let i = 0; i < a.length; i++) {
@@ -18,3 +55,4 @@ const add = (a, b) =>
       }
     }
   });
+*/
